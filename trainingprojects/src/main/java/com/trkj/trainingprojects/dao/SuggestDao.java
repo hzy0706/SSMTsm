65eed694077a7e9c3dae115e7b14fb4b34c4fdaf
hzy0706 <85111595+hzy0706@users.noreply.github.com>
2021-06-07 08:35:48 +0800

    修改Suggest报错问题

--- trainingprojects/src/main/java/com/trkj/trainingprojects/dao/SuggestDao.java
+++ trainingprojects/src/main/java/com/trkj/trainingprojects/dao/SuggestDao.java
@@ -1,11 +1,7 @@
 package com.trkj.trainingprojects.dao;
 
-<<<<<<< HEAD
-
 import com.trkj.trainingprojects.vo.SuggestVo;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import java.util.List;
 
 /**
  * 意见箱表(Suggest)表数据库访问层
@@ -16,49 +12,15 @@ import java.util.List;
 @Mapper
 public interface SuggestDao {
 
-    /**
-     * 通过ID查询单条数据
-     *
-     * @param suggestId 主键
-     * @return 实例对象
-     */
-    SuggestVo queryById(Integer suggestId);
-
-    /**
-     * 新增数据
-     *
-     * @param suggest 实例对象
-     * @return 影响行数
-     */
-    int addSuggest(SuggestVo suggest);
-
-    /**
-     * 修改数据
-     *
-     * @param suggest 实例对象
-     * @return 影响行数
-     */
-    int update(SuggestVo suggest);
-    /**
-     * 查询所有
-     */
-    List<SuggestVo> selectAll();
-=======
-import com.trkj.trainingprojects.entity.Suggest;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface SuggestDao {
     int deleteByPrimaryKey(Integer suggestId);
 
-    int insert(Suggest record);
+    int insert(SuggestVo record);
 
-    int insertSelective(Suggest record);
+    int insertSelective(SuggestVo record);
 
-    Suggest selectByPrimaryKey(Integer suggestId);
+    SuggestVo selectByPrimaryKey(Integer suggestId);
 
-    int updateByPrimaryKeySelective(Suggest record);
+    int updateByPrimaryKeySelective(SuggestVo record);
 
-    int updateByPrimaryKey(Suggest record);
->>>>>>> c649abe70ff4d67687954119f8437cdbd1c9bc3e
+    int updateByPrimaryKey(SuggestVo record);
 }
