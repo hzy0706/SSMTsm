7e2050dabcdfe0491234b03c3d1892851fd4cae9
cjkchn <85102216+cjkchn@users.noreply.github.com>
2021-06-15 16:23:17 +0800

    删除json

--- trainingprojects/src/main/java/com/trkj/trainingprojects/controller/SemesterController.java
+++ trainingprojects/src/main/java/com/trkj/trainingprojects/controller/SemesterController.java
@@ -1,7 +1,5 @@
 package com.trkj.trainingprojects.controller;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.trkj.trainingprojects.service.SemesterService;
