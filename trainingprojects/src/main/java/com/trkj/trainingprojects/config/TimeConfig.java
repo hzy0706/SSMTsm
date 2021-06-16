028fffa8632bdb362c5e9bdf24e983236c0ab137
hzy0706 <85111595+hzy0706@users.noreply.github.com>
2021-06-16 10:49:32 +0800

    完善日期显示格式

--- trainingprojects/src/main/java/com/trkj/trainingprojects/config/TimeConfig.java
+++ trainingprojects/src/main/java/com/trkj/trainingprojects/config/TimeConfig.java
@@ -1,4 +1,5 @@
 package com.trkj.trainingprojects.config;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,10 +14,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class TimeConfig {
     @Bean
     public MappingJackson2HttpMessageConverter getMappingJackson2HttpMessageConverter() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
         //设置日期格式
         ObjectMapper objectMapper = new ObjectMapper();
-
+        objectMapper.setDateFormat(dateFormat);
         mappingJackson2HttpMessageConverter.setObjectMapper(objectMapper);
         //设置中文编码格式
         List<MediaType> list = new ArrayList<MediaType>();
